e201470b662afa2d89128a0437d82c9fbbdcab4b
Giap2k4 <112042343+Giap2k4@users.noreply.github.com>
2022-08-29 17:43:41 +0700

    Create Bai6.6.c

---
+++ lab02/Bai6.6.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+}
