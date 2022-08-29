89635848ba11c55ed432340935b303e4745b0480
Giap2k4 <112042343+Giap2k4@users.noreply.github.com>
2022-08-29 17:43:32 +0700

    Create Bai6.8.c

---
+++ lab02/Bai6.8.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+	int i;
+	float x;
+	char c;
+	printf("i : %d\n");
+	scanf("%3d ", &i);
+	printf("float x : %d\n");
+	scanf("%5f", &x);
+	printf("char c : %d");
+	scanf("%c", &c);
+	
+}
