cbb4e73bca52fef5d949d333433b4dca89abe732
Giap2k4 <112042343+Giap2k4@users.noreply.github.com>
2022-08-29 17:43:45 +0700

    Create Bai6.5.c

---
+++ lab02/Bai6.5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+
+{
+	int a = 80;
+	char b = 'C';
+	
+	printf("This is the number stored in 'a' %d\n", a);
+	printf("This is a character interpreter from 'a' %c\n", a);
+	printf("This is also a character stored in 'b' %c\n", b);
+	printf("Hey! the character stored in 'b' is printed as a number! %d", b);
+	
+}
