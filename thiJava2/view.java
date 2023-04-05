19b75ce5e4e1c9a458948b7e0d53a7a53b2232ea
Giap2k4 <112042343+Giap2k4@users.noreply.github.com>
2023-04-05 10:21:10 +0700

    thiJava2

---
+++ thiJava2/view.java
@@ -0,0 +1,29 @@
+package thi2;
+
+import java.util.Scanner;
+
+public class view {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("1. Add student records\n2. Display student records\n3. Save\n4. Exit");
+        controllerStudent ctt = new controllerStudent();
+        while (true){
+            System.out.printf("Nhập lựa chọn: ");
+            int a = sc.nextInt();
+            sc.nextLine();
+            switch (a){
+                case 1:
+                    ctt.add();
+                    break;
+                case 2:
+                    ctt.display();
+                    break;
+                case 3:
+                    ctt.save();
+                    break;
+                case 4:
+                    return;
+            }
+        }
+    }
+}
