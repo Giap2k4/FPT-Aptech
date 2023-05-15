7beb2c22dd0b774de01fb07de51acaa7ad04930d
Giap2k4 <112042343+Giap2k4@users.noreply.github.com>
2023-05-15 11:51:14 +0700

    upC#

---
+++ C#/Program.cs
@@ -0,0 +1,35 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        Atomic atm = new Atomic();
+        int i = 0;
+        System.Console.WriteLine("1. Enter atom: \n2. Atomic display: ");
+        do
+        {
+            i++;
+            System.Console.WriteLine("Input change: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            switch(x)
+            {
+                case 1:
+                    atm.accept();
+                    break;
+                case 2:
+                    atm.display();
+                    return;
+                default:
+                    System.Console.WriteLine("Enter the wrong selection");
+                    break;
+
+            }
+        } while (i != 10);
+        if(i == 10)
+        {
+            atm.display();
+        }
+        
+        
+        
+    }
+}
