0349bb5fb9841bdbbc45b1cb5b09b3540149a1c3
Giap2k4 <112042343+Giap2k4@users.noreply.github.com>
2023-05-15 11:25:24 +0700

    C#

---
+++ C#/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        Atomic atm = new Atomic();
+        int i = 0;
+
+        do
+        {
+            i++;
+            atm.accept();
+            
+        } while (i != 3);
+        System.Console.WriteLine("------------------------------------ ");
+        atm.display();
+
+    }
+}
