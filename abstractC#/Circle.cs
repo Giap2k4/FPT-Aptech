b2d987a1f06049b238f5e23cb8213cb5659622ac
Giap2k4 <112042343+Giap2k4@users.noreply.github.com>
2023-05-24 08:44:33 +0700

    abstractC#

---
+++ abstractC#/Circle.cs
@@ -0,0 +1,28 @@
+﻿class Circle : GeometricObject
+{
+    private double radius;
+
+    public Circle(double radius,string color, double weight) : base(color, weight)
+    {
+        this.radius = radius;
+    }
+
+    public override double findArea()
+    {
+        double Area = radius * radius * Math.PI;
+        Console.WriteLine(Area);
+        return Area;
+    }
+
+    public override double findPerimeter()
+    {
+        double Perimeter = 2 * radius * Math.PI;
+        Console.WriteLine(Perimeter);
+        return Perimeter;
+    }
+
+    public override string? ToString()
+    {
+        return "Circle has: radius is: "+radius+", color is: "+PColor+", weigth is: "+PWeight;
+    }
+}
---
+++ abstractC#/GeometricObject.cs
@@ -0,0 +1,25 @@
+﻿abstract class GeometricObject
+{
+    private string color;
+    private double weight;
+
+    protected GeometricObject(string color, double weight)
+    {
+        this.color = color;
+        this.weight = weight;
+    }
+    public string PColor
+    {
+        get { return color; }
+        set { color = value; }
+    }
+
+    public double PWeight
+    {
+        get { return weight; }
+        set { weight = value; }
+    }
+
+    public abstract double findArea();
+    public abstract double findPerimeter();
+}
